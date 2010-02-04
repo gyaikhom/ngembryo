56b8c847b7db34d3887a0ebbb874fd8bfd6b25a1
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-02-04 01:04:08 +0000

    * Text areas in forms are now break free and length limited. * Updated styles for the forms dialog.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@38 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- c2dm.php
+++ c2dm.php
@@ -1,19 +1,19 @@
 <form dojoType='dijit.form.Form' name='c2dm' id='c2dm' method='post'>
-    X: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='x' id='x' value='<?php echo ($_GET[x]); ?>'>
+    X: <input type='text' trim='true' name='x' id='x' value='<?php echo ($_GET[x]); ?>'>
+    Y: <input type='text' trim='true' name='y' id='y' value='<?php echo ($_GET[y]); ?>'>
     <br>
-    Y: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='y' id='y' value='<?php echo ($_GET[y]); ?>'>
+    Scale: <input type='text' trim='true' name='scale' id='scale' value='<?php echo ($_GET[scale]); ?>'>
+    Distance: <input type='text' trim='true' name='dst' id='dst' value='<?php echo ($_GET[dst]); ?>'>
     <br>
-    Scale: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='scale' id='scale' value='<?php echo ($_GET[scale]); ?>'>
-    Distance: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='dst' id='dst' value='<?php echo ($_GET[dst]); ?>'>
+    Yaw: <input type='text' trim='true' name='yaw' id='yaw' value='<?php echo ($_GET[yaw]); ?>'>
+    Roll: <input type='text' trim='true' name='rol' id='rol' value='<?php echo ($_GET[rol]); ?>'>
+    Pitch: <input type='text' trim='true' name='pit' id='pit' value='<?php echo ($_GET[pit]); ?>'>
     <br>
-    Yaw: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='yaw' id='yaw' value='<?php echo ($_GET[yaw]); ?>'>
-    Roll: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='rol' id='rol' value='<?php echo ($_GET[rol]); ?>'>
-    Pitch: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='pit' id='pit' value='<?php echo ($_GET[pit]); ?>'>
+    Label (maximum 40 characters):<br>
+    <input type='text' trim='true' name='label' id='label' maxlength='40' value='' style='width: 300px;'>
     <br>
-    Label: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='label' id='label' maxlength='255' value='' style='width: 100%;'>
-    <br>
-    Description:
-    <textarea dojoType="dijit.form.Textarea" name='description' id='description'></textarea>
+    Description (maximum 400 characters, no line breaks):<br>
+    <textarea name='description' id='description' rows="10" cols="75" onKeyDown="reformatTextArea(this, 400);" onKeyUp="reformatTextArea(this, 400);"></textarea>
     <br>
     <button dojoType="dijit.form.Button" type="submit">
         OK
--- c2dr.php
+++ c2dr.php
@@ -1,17 +1,18 @@
 <form dojoType='dijit.form.Form' name='c2dr' id='c2dr' method='post'>
 	Polyline: <input type='text' name='polyline' id='polyline' value='<?php echo ($_GET[polyline]); ?>'>
 	<br>
-    Scale: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='scale' id='scale' value='<?php echo ($_GET[scale]); ?>'>
-    Distance: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='dst' id='dst' value='<?php echo ($_GET[dst]); ?>'>
+    Scale: <input type='text' trim='true' name='scale' id='scale' value='<?php echo ($_GET[scale]); ?>'>
+    Distance: <input type='text' trim='true' name='dst' id='dst' value='<?php echo ($_GET[dst]); ?>'>
     <br>
-    Yaw: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='yaw' id='yaw' value='<?php echo ($_GET[yaw]); ?>'>
-    Roll: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='rol' id='rol' value='<?php echo ($_GET[rol]); ?>'>
-    Pitch: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='pit' id='pit' value='<?php echo ($_GET[pit]); ?>'>
+    Yaw: <input type='text' trim='true' name='yaw' id='yaw' value='<?php echo ($_GET[yaw]); ?>'>
+    Roll: <input type='text' trim='true' name='rol' id='rol' value='<?php echo ($_GET[rol]); ?>'>
+    Pitch: <input type='text' trim='true' name='pit' id='pit' value='<?php echo ($_GET[pit]); ?>'>
     <br>
-    Label: <input type='text' dojoType='dijit.form.TextBox' trim='true' name='label' id='label' maxlength='255' value='' style='width: 100%;'>
+    Label (maximum 40 characters):<br>
+    <input type='text' trim='true' name='label' id='label' maxlength='40' value='' style='width: 300px;'>
     <br>
-    Description:
-    <textarea dojoType="dijit.form.Textarea" name='description' id='description'></textarea>
+    Description (maximum 400 characters, no line breaks):<br>
+    <textarea name='description' id='description' rows="10" cols="75" onKeyDown="reformatTextArea(this, 400);" onKeyUp="reformatTextArea(this, 400);"></textarea>
     <br>
     <button dojoType="dijit.form.Button" type="submit">
         OK
