e87fbc40e9577da55a0c91eb74a0e197a7877e62
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-03-17 19:06:11 +0000

    Added new javascript modules.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@80 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- lib/ngembryo/combine_compress.sh
+++ lib/ngembryo/combine_compress.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 echo "" > ng.js;
-for x in annotation icon iconManager layerManager labelRenderer marker2D marker3D region2D engine cookieManager ngembryoWoolz IIPManager dialogManager toolbar ngembryo;
+for x in utils models annotation icon iconManager layerManager labelRenderer marker2D region2D engine cookieManager overlays IIPManager dialogManager orientations layers resources toolbar controlManager tipper content ngembryo;
   do
   sed -e "s/console.info/\/\/console.info/g" $x.js | sed -e "s/console.log/\/\/console.log/g" | sed -e "s/console.warn/\/\/console.warn/g" | sed -e "s/console.debug/\/\/console.debug/g" >> ng.js;
 done;
 
 # Download yuicompressor from http://developer.yahoo.com/yui/compressor/
 java -jar ~/Downloads/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar ng.js -o ngc.js;
+
