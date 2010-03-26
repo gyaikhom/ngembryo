8bf38283131e58321312e967c7124f2c993e2773
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-03-26 08:57:04 +0000

    * Added compressed NGEmbryo javascript generation script with header. * Added YUI compressor.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@88 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- lib/ngembryo/combine_compress.sh
+++ lib/ngembryo/combine_compress.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
 echo "" > ng.js;
+
 for x in utils models annotation icon iconManager layerManager labelRenderer marker2D region2D engine cookieManager overlays IIPManager dialogManager orientations layers resources toolbar controlManager tipper content ngembryo;
   do
   sed -e "s/console.info/\/\/console.info/g" $x.js | sed -e "s/console.log/\/\/console.log/g" | sed -e "s/console.warn/\/\/console.warn/g" | sed -e "s/console.debug/\/\/console.debug/g" >> ng.js;
 done;
 
 # Download yuicompressor from http://developer.yahoo.com/yui/compressor/
-java -jar ~/Downloads/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar ng.js -o ngc.js;
+java -jar yuicompressor-2.4.2.jar ng.js -o temp.js;
 
+cat header.js temp.js > ngc.js
+
+rm temp.js;
+rm ng.js;
