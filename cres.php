6cf637c6f5718d1743883120b2bdd484fc42ac9c
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-05-05 13:31:08 +0000

    Increased number of characters allowed in resource title.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@108 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- cres.php
+++ cres.php
@@ -1,8 +1,8 @@
 <div class="form">
 <form dojoType='dijit.form.Form' name='cres' id='cres' method='post'>
     Author: <input type='text' trim='true' name='author' id='author' value='' style='width: 100%;'><br>
-    Title (maximum 40 characters):<br>
-    <input type='text' trim='true' name='title' id='title' maxlength='40' value='' style='width: 100%;'><br>
+    Title (maximum 100 characters):<br>
+    <input type='text' trim='true' name='title' id='title' maxlength='100' value='' style='width: 100%;'><br>
     URI: <input type='text' trim='true' name='link' id='link' value='http://' style='width: 100%;'><br>
     Abstract (maximum 400 characters, no line breaks):<br>
     <textarea name='description' id='description' rows="10" cols="75" onKeyDown="reformatTextArea(this, 400);" onKeyUp="reformatTextArea(this, 400);"></textarea><br><br>
