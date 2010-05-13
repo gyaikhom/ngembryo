46e69009a7f5a516da5ae2530e71addb30023d06
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-05-13 19:19:02 +0000

    Added fadein/fadeout feedback.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@129 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- lib/ngembryo/combine_compress.sh
+++ lib/ngembryo/combine_compress.sh
@@ -2,7 +2,7 @@
 
 echo "" > ng.js;
 
-for x in utils models annotation icon iconManager layerManager labelRenderer marker2D region2D engine cookieManager overlays IIPManager dialogManager orientations layers resources toolbar controlManager tipper content ngembryo;
+for x in utils models annotation icon iconManager layerManager labelRenderer marker2D region2D engine cookieManager overlays IIPManager dialogManager orientations layers resources toolbar controlManager tipper feedback content ngembryo;
   do
   sed -e "s/console.info/\/\/console.info/g" $x.js | sed -e "s/console.log/\/\/console.log/g" | sed -e "s/console.warn/\/\/console.warn/g" | sed -e "s/console.debug/\/\/console.debug/g" >> ng.js;
 done;
