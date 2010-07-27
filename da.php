4a4fac5dc149a510723861b4c7e327750756fd5e
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-07-27 14:39:14 +0000

    Delete annotation: code refinement and session requirement.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@176 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- da.php
+++ da.php
@@ -1,36 +1,67 @@
 <?php
-$con = mysql_connect("localhost", "ngembryo", "ngembryo");
-if (!$con) {
-	die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).'}');
+/**
+ * @projectDescription The Next-Generation Embryology Project
+ *
+ * School of Informatics, University of Edinburgh Funded by the JISC
+ * (http://www.jisc.ac.uk/)
+ *
+ * @author gyaikhom
+ *
+ * @description Delete annotation.
+ */
+
+include 'login.php';
+
+function die_error($c, $m) {
+	die('{success: false, errcode: '.$c.', message: "'.$m.'"}');
 }
 
-mysql_select_db("ngembryo", $con);
+function echo_success($m) {
+	echo '{success: true, errcode: 0, message: "'.$m.'"}';
+}
 
-$aid = $_GET[aid];
-$type = $_GET[type];
-$table = "";
-
-if ($type == "m") {
-	$table = "2Dmarker";
-} else {
-	if ($type == "r") {
-		$table = "2Dregion";
-	} else {
-		die('{success: false, errcode: 2, message: "Unknown annotation type."}');
+/* Delete annotation. */
+function delete_annotation($table, $aid) {
+	global $con;
+	$sql = "UPDATE $table SET deleted_at=NOW() WHERE id=$aid";
+	if (!mysql_query($sql, $con)) {
+		die_error(-1, json_encode(mysql_error()));
 	}
 }
 
-$sql = "UPDATE $table SET deleted_at=NOW() WHERE id=$aid";
-if (!mysql_query($sql, $con)) {
-	die('{success: false, errcode: -1, message: '.json_encode(mysql_error()).'}');
+/* Unlink resources from the annotation. */
+function unlink_annotation_resources($table, $aid) {
+	global $con;
+	$table = $table.Resource;
+	$sql = "UPDATE $table SET deleted_at=NOW() WHERE annotation_id=$aid";
+	if (!mysql_query($sql, $con)) {
+		die_error(-2, json_encode(mysql_error()));
+	}
 }
 
-$table = $table.Resource;
-$sql = "UPDATE $table SET deleted_at=NOW() WHERE annotation_id=$aid";
-if (!mysql_query($sql, $con)) {
-	die('{success: false, errcode: -2, message: '.json_encode(mysql_error()).'}');
+$logged_in = checkLogin();
+if (!$logged_in) {
+	header('Location: ngembryo.php');
+} else {
+
+	/* Supplied by the client. */
+	$aid = $_GET[aid];
+	$type = $_GET[type];
+	$table = "";
+
+	if ($type == "m") {
+		$table = "2Dmarker";
+	} else {
+		if ($type == "r") {
+			$table = "2Dregion";
+		} else {
+			die_error(-3, "Unknown annotation type.");
+		}
+	}
+	delete_annotation($table, $aid);
+	unlink_annotation_resources($table, $aid);
+	echo_success("Annotation has been deleted.");
 }
 
-echo '{success: true, errcode: 0, message: "Annotation has been deleted."}';
 mysql_close($con);
 ?>
