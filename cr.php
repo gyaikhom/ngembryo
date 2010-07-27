ef327916aefe74b971e568d2995a91c076e7d10d
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-07-27 13:54:13 +0000

    Create region: code refinement and session requirement. Bug fix: Incorrect bounding box top-left corner calculation.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@173 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- cr.php
+++ cr.php
@@ -1,97 +1,147 @@
 <?php
+/**
+ * @projectDescription The Next-Generation Embryology Project
+ *
+ * School of Informatics, University of Edinburgh Funded by the JISC
+ * (http://www.jisc.ac.uk/)
+ *
+ * @author gyaikhom
+ *
+ * @description Create a new region annotation.
+ */
 
-include 'utils.php';
+include 'login.php';
 
-$con = mysql_connect("localhost", "ngembryo", "ngembryo");
-if (!$con) {
-	die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', id: 0}');
+function die_error($c, $m) {
+	die('{success: false, errcode: '.$c.', message: "'.$m.'", rid: 0}');
 }
 
-mysql_select_db("ngembryo", $con);
+function echo_success($m, $rid) {
+	echo '{success: true, errcode: 0, message: "'.$m.'", rid:'.$rid.'}';
+}
 
-function cleanup($con, $region_id, $reason) {
-	$err_msg = mysql_error();
+/* In case of failure, remove the region and associated polyline points. */
+function cleanup($rid, $reason) {
+	global $con;
 
 	/* Remove the region which we inserted just now. */
-	$sql_remove_region = "DELETE FROM 2Dregion WHERE deleted_at IS NULL AND id = '$region_id'";
-	mysql_query($sql_remove_region, $con);
+	$sql = "DELETE FROM 2Dregion WHERE deleted_at IS NULL AND id = '$rid'";
+	if (!mysql_query($sql, $con)) {
+		die_error(-1, json_encode(mysql_error()));
+	}
 
 	/* Remove any existing polyline point associated with this region. */
-	$sql_remove_region = "DELETE FROM 2Dpolyline WHERE deleted_at IS NULL AND 2Dregion_id = '$region_id'";
-	mysql_query($sql_remove_region, $con);
+	$sql = "DELETE FROM 2Dpolyline WHERE deleted_at IS NULL AND 2Dregion_id = '$rid'";
+	if (!mysql_query($sql, $con)) {
+		die_error(-2, json_encode(mysql_error()));
+	}
 
-	die('{success: false, errcode: -1, message: "Failed to insert polyline: '.$reason.'", id: 0}');
+	die_error(-3, "Failed to insert polyline: '$reason'");
 }
 
-/* Supplied by the client. */
-$scale = $_POST[scale];
-$label = $_POST[label];
-$description = $_POST[description];
-$lid = $_POST[lid];
-$polyline = $_POST[polyline];
-
-/* Escape quotes etc. */
-$label = return_well_formed($label);
-$description = return_well_formed($description);
-
-/* First check if the layer exists. */
-$layer = mysql_query("SELECT id FROM layer WHERE deleted_at IS NULL AND id=$lid");
-if ($temp = mysql_fetch_array($layer)) {
-	$lid = $temp['id'];
-} else {
-	die('{success: false, errcode: -2, message: "Invalid layer.", id: 0}');
+/* Create a region. */
+function create_region($lid, $s, $l, $d) {
+	global $con;
+	$sql = "INSERT INTO 2Dregion (layer_id, scale, tl_x, tl_y, br_x, br_y, label, description, created_at) VALUES ('$lid', '$s', 0, 0, 0, 0, '$l', '$d', NOW())";
+	if (!mysql_query($sql, $con)) {
+		die_error(-4, json_encode(mysql_error()));
+	}
+	return mysql_insert_id();
 }
 
-/*
- * Parse the polyline before making an insertion.
- */
-if (isset($polyline)) {
-	$points = explode(':', $polyline);
-	if (sizeof($points) < 3) {
-		die('{success: false, errcode: -3, message: "At least three points are required to create a region.", id: 0}');
-	} else {
-		/* Create a 2D region. */
-		$sql_insert_region = "INSERT INTO 2Dregion (layer_id, scale, tl_x, tl_y, br_x, br_y, label, description, created_at) VALUES ('$lid', '$scale', 0, 0, 0, 0, '$label', '$description', NOW())";
-		if (!mysql_query($sql_insert_region, $con)) {
-			die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', id: 0}');
+/* Check if a layer with the given id exists. */
+function check_layer($lid) {
+	global $con;
+	$sql = "SELECT id FROM layer WHERE deleted_at IS NULL AND id=$lid LIMIT 1";
+	if ($temp = mysql_query($sql, $con)) {
+		if (mysql_num_rows($temp) > 0) {
+			return true;
+		} else {
+			return false;
 		}
-		$region_id = mysql_insert_id();
+	} else {
+		die_error(-5, json_encode(mysql_error()));
+	}
+}
 
-		/* Insert the polyline associated with the 2D region. */
-		$tl_x = 0;
-		$tl_y = 0;
-		$br_x = 0;
-		$br_y = 0;
-		for ($i = 0; $i < count($points); $i++) {
-			$point = explode(',', $points[$i]);
-			if (count($point) != 2)
-			cleanup($con, $region_id, "invalid point".$points[$i]);
-			else {
-				$sql_insert_point = "INSERT INTO 2Dpolyline (x, y, 2Dregion_id, rank, created_at) VALUES ('$point[0]', '$point[1]', '$region_id', '$i', NOW())";
+/* Update the 2D region entry with the correct bounding box coordinates. */
+function update_bbox($rid, $tl_x, $tl_y, $br_x, $br_y) {
+	global $con;
+	$sql = "UPDATE 2Dregion SET tl_x=$tl_x, tl_y=$tl_y, br_x=$br_x, br_y=$br_y WHERE id=$rid";
+	if (!mysql_query($sql, $con)) {
+		cleanup($rid, "region could not be updated.");
+	}
+}
 
-				/* If the polyline insertion was unsuccessful, clean up. */
-				if (!mysql_query($sql_insert_point, $con))
-				cleanup($con, $region_id, "point could not be inserted.");
+/* Insert the polyline associated with the 2D region. */
+function insert_polyline($rid, $points) {
+	global $con;
+	$bbox = array("tl_x" => 999999, "tl_y" => 999999, "br_x" => 0, "br_y" => 0);
+	for ($i = 0; $i < count($points); $i++) {
+		$point = explode(',', $points[$i]);
+		if (count($point) != 2) {
+			cleanup($rid, "invalid point".$points[$i]);
+		} else {
+			$sql = "INSERT INTO 2Dpolyline (x, y, 2Dregion_id, rank, created_at) VALUES ('$point[0]', '$point[1]', '$rid', '$i', NOW())";
 
-				/* Update the top-left and bottom-right bounding box. */
-				if ($point[0] < $tl_x) $tl_x = $point[0];
-				else if ($point[0] > $br_x) $br_x = $point[0];
+			/* If the polyline insertion was unsuccessful, clean up. */
+			if (!mysql_query($sql, $con)) {
+				cleanup($rid, "point could not be inserted.");
+			}
 
-				if ($point[1] < $tl_y) $tl_y = $point[1];
-				else if ($point[1] > $br_y) $br_y = $point[1];
+			/* Update the top-left of  bounding box. */
+			if ($point[0] < $bbox["tl_x"]) {
+				$bbox["tl_x"] = $point[0];
+			} else if ($point[0] > $bbox["br_x"]) {
+				$bbox["br_x"] = $point[0];
+			}
+
+			/* Update the bottom-right of bounding box. */
+			if ($point[1] < $bbox["tl_y"]) {
+				$bbox["tl_y"] = $point[1];
+			}
+			else if ($point[1] > $bbox["br_y"]) {
+				$bbox["br_y"] = $point[1];
 			}
 		}
-
-		/* Update the 2D region entry with the correct bounding box coordinates. */
-		$sql_update_regionbb = "UPDATE 2Dregion SET tl_x = $tl_x, tl_y = $tl_y, br_x = $br_x, br_y = $br_y WHERE id = $region_id";
-		if (!mysql_query($sql_update_regionbb, $con)) {
-			cleanup($con, $region_id, "region could not be updated.");
-		}
 	}
-} else {
-	die('{success: false, errcode: -4, message: "Supplied polyline is invalid: '.$reason.'", id: 0}');
+	return $bbox;
 }
-echo '{success: true, errcode: 0, message: "New region \''.$label.'\' has been created.", id:'.$region_id.'}';
+
+$logged_in = checkLogin();
+if (!$logged_in) {
+	header('Location: ngembryo.php');
+} else {
+
+	/* Supplied by the client. */
+	$scale = $_POST[scale];
+	$label = $_POST[label];
+	$description = $_POST[description];
+	$lid = $_POST[lid];
+	$polyline = $_POST[polyline];
+
+	/* Escape quotes etc. */
+	$label = return_well_formed($label);
+	$description = return_well_formed($description);
+
+	if (check_layer($lid)) {
+		if (isset($polyline)) {
+			$points = explode(':', $polyline);
+			if (sizeof($points) < 3) {
+				die_error(-6, "At least three points are required to create a region.");
+			} else {
+				$rid = create_region($lid, $scale, $label, $description);
+				$bbox = insert_polyline($rid, $points);
+				update_bbox($rid, $bbox["tl_x"], $bbox["tl_y"], $bbox["br_x"], $bbox["br_y"]);
+				echo_success("New region \'$label\' has been created.", $rid);
+			}
+		} else {
+			die_error(-7, "Supplied polyline is invalid.");
+		}
+	} else {
+		die_error(-8, "Invalid layer.");
+	}
+}
 
 mysql_close($con);
 ?>
