2f7f2d0f59c380400800c979ecfb3058b6cae80a
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-07-27 14:25:07 +0000

    Create resource: code refinement and session requirement.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@175 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- crs.php
+++ crs.php
@@ -1,54 +1,88 @@
 <?php
+/**
+ * @projectDescription The Next-Generation Embryology Project
+ *
+ * School of Informatics, University of Edinburgh Funded by the JISC
+ * (http://www.jisc.ac.uk/)
+ *
+ * @author gyaikhom
+ *
+ * @description Create a new resource.
+ */
 
-include 'utils.php';
+include 'login.php';
 
-$con = mysql_connect("localhost", "ngembryo", "ngembryo");
-if (!$con) {
-	die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', rid: 0}');
+function die_error($c, $m) {
+	die('{success: false, errcode: '.$c.', message: "'.$m.'", rid: 0}');
 }
 
-mysql_select_db("ngembryo", $con);
-
-/* Supplied by the client. */
-$title = $_POST[title];
-$author = $_POST[author];
-$link = $_POST[link]; /* New requirement: Just a flat list of resources. */
-$abstract = $_POST[description];
-
-/* Escape quotes etc. */
-$title = return_well_formed($title);
-$author = return_well_formed($author);
-$abstract = return_well_formed($abstract);
+function echo_success($m, $rid) {
+	echo '{success: true, errcode: 0, message: "'.$m.'", rid:'.$rid.'}';
+}
 
 /* Check if a resource with the given title and author exists. */
-$sql = "SELECT * FROM resource WHERE deleted_at IS NULL AND author='$author' AND title='$title'";
-if ($result = mysql_query($sql, $con)) {
-	if ($temp = mysql_fetch_array($result)) {
-		die('{success: false, errcode: -2, message: "Resource \''.$title.'\' already exists. No new resource created.", rid: '.$temp['id'].'}');
+function check_resource($a, $t) {
+	global $con;
+	$sql = "SELECT id FROM resource WHERE deleted_at IS NULL AND author='$a' AND title='$t' LIMIT 1";
+	if ($temp = mysql_query($sql, $con)) {
+		if (mysql_num_rows($temp) > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		die_error(-1, json_encode(mysql_error()));
 	}
-} else {
-	die('{success: false, errcode: 3, message: '.json_encode(mysql_error()).', rid: 0}');
 }
 
 /* Create a new resource. */
-$sql = "INSERT INTO resource (title, author, abstract, created_at) VALUES ('$title', '$author', '$abstract', NOW())";
-if (!mysql_query($sql, $con)) {
-	die('{success: false, errcode: 6, message: '.json_encode(mysql_error()).', rid: 0}');
+function create_resource($t, $a, $ab) {
+	global $con;
+	$sql = "INSERT INTO resource (title, author, abstract, created_at) VALUES ('$t', '$a', '$ab', NOW())";
+	if (!mysql_query($sql, $con)) {
+		die_error(-2, json_encode(mysql_error()));
+	}
+	return mysql_insert_id();
 }
-$rid = mysql_insert_id();
 
 /* Create a new resource item.
- * 
+ *
  * NOTE: This is according to the new requirement from April 30 meeting at NCL.
  * We have attempted to minimise changes to the existing database. So, every resource will
  * now have only one resource item. User cannot add new resource items.
  */
-$sql = "INSERT INTO resourceItem (resource_id, title, abstract, link, created_at) VALUES ('$rid', '$title', '$abstract', '$link', NOW())";
-if (!mysql_query($sql, $con)) {
-    die('{success: false, errcode: 6, message: '.json_encode(mysql_error()).', rid: 0}');
+function create_resource_item($rid, $t, $a, $l) {
+	global $con;
+	$sql = "INSERT INTO resourceItem (resource_id, title, abstract, link, created_at) VALUES ('$rid', '$t', '$a', '$l', NOW())";
+	if (!mysql_query($sql, $con)) {
+		die_error(-3, json_encode(mysql_error()));
+	}
 }
 
-echo '{success: true, errcode: 0, message: "New resource \''.$title.'\' has been created.", rid:'.$rid.'}';
+$logged_in = checkLogin();
+if (!$logged_in) {
+	header('Location: ngembryo.php');
+} else {
+
+	/* Supplied by the client. */
+	$title = $_POST[title];
+	$author = $_POST[author];
+	$link = $_POST[link]; /* New requirement: Just a flat list of resources. */
+	$abstract = $_POST[description];
+
+	/* Escape quotes etc. */
+	$title = return_well_formed($title);
+	$author = return_well_formed($author);
+	$abstract = return_well_formed($abstract);
+
+	if (check_resource($author, $title)) {
+		die_error(-4, "Resource \'$title\' by \'$author\' already exists. No new resource created.");
+	} else {
+		$rid = create_resource($title, $author, $abstract);
+		create_resource_item($rid, $title, $abstract, $link);
+		echo_success("New resource \'$title\' has been created.", $rid);
+	}
+}
 
 mysql_close($con);
 ?>
