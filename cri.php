a59628554e9eb9f45fb954cba0bb12c3283945ee
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-07-27 14:13:33 +0000

    Create resource item: code refinement and session requirement.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@174 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- cri.php
+++ cri.php
@@ -1,54 +1,93 @@
 <?php
+/**
+ * @projectDescription The Next-Generation Embryology Project
+ *
+ * School of Informatics, University of Edinburgh Funded by the JISC
+ * (http://www.jisc.ac.uk/)
+ *
+ * @author gyaikhom
+ *
+ * @description Create a resource item.
+ */
 
-include 'utils.php';
+include 'login.php';
 
-$con = mysql_connect("localhost", "ngembryo", "ngembryo");
-if (!$con) {
-	die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', rid: 0}');
+function die_error($c, $m) {
+	die('{success: false, errcode: '.$c.', message: "'.$m.'", rid: 0}');
 }
 
-mysql_select_db("ngembryo", $con);
-
-/* Supplied by the client. */
-$rid = $_POST[rid];
-$title = $_POST[title];
-$abstract = $_POST[description];
-$mime = $_POST[mime];
-$link = $_POST[link];
-
-/* Escape quotes etc. */
-$title = return_well_formed($title);
-$abstract = return_well_formed($abstract);
-$mime = return_well_formed($mime);
-$link = return_well_formed($link);
+function echo_success($m, $rid) {
+	echo '{success: true, errcode: 0, message: "'.$m.'", rid:'.$rid.'}';
+}
 
 /* Check if the resource exists. */
-$sql = "SELECT * FROM resource WHERE deleted_at IS NULL AND id='$rid'";
-if ($result = mysql_query($sql, $con)) {
-	if (!mysql_fetch_array($result)) {
-		die('{success: false, errcode: -2, message: "Resource does not exists. No new resource item can be created.", rid: 0}');
+function check_resource($rid) {
+	global $con;
+	$sql = "SELECT id FROM resource WHERE deleted_at IS NULL AND id='$rid' LIMIT 1";
+	if ($temp = mysql_query($sql, $con)) {
+		if (mysql_num_rows($temp) > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		die_error(-1, json_encode(mysql_error()));
 	}
-} else {
-	die('{success: false, errcode: 3, message: '.json_encode(mysql_error()).', rid: 0}');
 }
 
 /* Check if a resource item with the given title, mime, and link exists. */
-$sql = "SELECT * FROM resourceItem WHERE deleted_at IS NULL AND resource_id='$rid' AND title='$title' AND mime='$mime' AND link='$link'";
-if ($result = mysql_query($sql, $con)) {
-	if ($temp = mysql_fetch_array($result)) {
-		die('{success: false, errcode: -2, message: "Resource item already exists. No new resource item created.", rid: '.$temp['id'].'}');
+function check_resource_item($rid, $t, $m, $l) {
+	global $con;
+	$sql = "SELECT id FROM resourceItem WHERE deleted_at IS NULL AND resource_id='$rid' AND title='$t' AND mime='$m' AND link='$l' LIMIT 1";
+	if ($temp = mysql_query($sql, $con)) {
+		if (mysql_num_rows($temp) > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		die_error(-2, json_encode(mysql_error()));
 	}
-} else {
-	die('{success: false, errcode: 3, message: '.json_encode(mysql_error()).', rid: 0}');
 }
 
 /* Create a new resource item. */
-$sql = "INSERT INTO resourceItem (resource_id, title, abstract, mime, link, created_at) VALUES ('$rid', '$title', '$abstract', '$mime', '$link', NOW())";
-if (!mysql_query($sql, $con)) {
-	die('{success: false, errcode: 6, message: '.json_encode(mysql_error()).', rid: 0}');
+function create_resource_item($rid, $t, $a, $m, $l) {
+	$sql = "INSERT INTO resourceItem (resource_id, title, abstract, mime, link, created_at) VALUES ('$rid', '$t', '$a', '$m', '$l', NOW())";
+	if (!mysql_query($sql, $con)) {
+		die_error(-3, json_encode(mysql_error()));
+	}
+	return mysql_insert_id();
 }
-$rid = mysql_insert_id();
-echo '{success: true, errcode: 0, message: "New resource item created.", rid:'.$rid.'}';
+
+$logged_in = checkLogin();
+if (!$logged_in) {
+	header('Location: ngembryo.php');
+} else {
+
+	/* Supplied by the client. */
+	$rid = $_POST[rid];
+	$title = $_POST[title];
+	$abstract = $_POST[description];
+	$mime = $_POST[mime];
+	$link = $_POST[link];
+
+	/* Escape quotes etc. */
+	$title = return_well_formed($title);
+	$abstract = return_well_formed($abstract);
+	$mime = return_well_formed($mime);
+	$link = return_well_formed($link);
+
+	if (check_resource($rid)) {
+		if (check_resource_item($rid, $title, $mime, $link)) {
+			die_error(-4, "Resource item exists. No new resource item created.");
+		} else {
+			$rid = create_resource_item($rid, $title, $abstract, $mime, $link);
+			echo_success("New resource item has been created.", $rid);
+		}
+	} else {
+		die_error(-5, "Resource does not exists. No new resource item can be created.");
+	}
+}
 
 mysql_close($con);
 ?>
