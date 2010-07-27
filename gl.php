eb8175fd8f0f2618d795e80e1a249fe878bb2d17
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-07-27 16:58:07 +0000

    Get layers: code refinement and session requirement.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@184 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- gl.php
+++ gl.php
@@ -1,57 +1,86 @@
 <?php
-$con = mysql_connect("localhost", "ngembryo", "ngembryo");
-if (!$con) {
-	if ($_GET[format] == "json") {
-		die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', layers: null}');
-	} else {
-		echo '<response><success>false</success><errcode>1</errcode><message>MySQL connection error:'.mysql_error().'</message><layers></layers></response>';
-	}
-}
-mysql_select_db("ngembryo", $con);
+/**
+ * @projectDescription The Next-Generation Embryology Project
+ *
+ * School of Informatics, University of Edinburgh Funded by the JISC
+ * (http://www.jisc.ac.uk/)
+ *
+ * @author gyaikhom
+ *
+ * @description Get layers.
+ */
 
-$model = $_GET[model];
-$yaw = $_GET[yaw];
-$pitch = $_GET[pitch];
-$roll = $_GET[roll];
-$distance = $_GET[distance];
+include 'login.php';
+
+function die_error($c, $m) {
+	die('{success:false,errcode:'.$c.',message:"'.$m.'",l:null}');
+}
+
+function echo_success($m, $l) {
+	echo '{success:true,errcode:0,message:"'.$m.'",l:'.$l.'}';
+}
 
 /* Find the orientation. */
-$sql = "SELECT * FROM orientation WHERE deleted_at IS NULL AND model_id='$model' AND yaw='$yaw' AND pitch='$pitch' AND roll='$roll' AND distance='$distance'";
-if ($result = mysql_query($sql, $con)) {
-	if ($x = mysql_fetch_array($result)) {
-		$orientation_id = $x['id'];
+function find_orientation($m, $y, $p, $r, $d) {
+	global $con;
+	$sql = "SELECT id FROM orientation WHERE deleted_at IS NULL AND model_id='$m' AND yaw='$y' AND pitch='$p' AND roll='$r' AND distance='$d' LIMIT 1";
+	if ($temp = mysql_query($sql, $con)) {
+		if ($x = mysql_fetch_array($temp)) {
+			return $x[0];
+		} else {
+			die_error(-1, "Orientation not found.");
+		}
 	} else {
-		die('{success: false, errcode: -3, message: "Found no orientation with the supplied parameters.", layers: null}');
+		die_error(-2, json_encode(mysql_error()));
 	}
-} else {
-	die('{success: false, errcode: 2, message: '.json_encode(mysql_error()).', layers: null}');
 }
 
 /* Find all of the layers for this orientation. */
-$sql = "SELECT * FROM layer WHERE deleted_at IS NULL AND orientation_id=$orientation_id";
-if (!($result = mysql_query($sql, $con))) {
-	if ($_GET[format] == "json") {
-		die('{success: false, errcode: 2, message: '.json_encode(mysql_error()).', layers: null}');
+function get_layers($oid) {
+	global $con;
+	$sql = "SELECT * FROM layer WHERE deleted_at IS NULL AND orientation_id=$oid";
+	if ($temp = mysql_query($sql, $con)) {
+		return $temp;
 	} else {
-		echo '<response><success>false</success><errcode>2</errcode><message>MySQL Query error:'.mysql_error().'</message><layers></layers></response>';
+		die_error(-3, json_encode(mysql_error()));
 	}
 }
 
-if ($_GET[format] == "json") {
-	echo '{success: true, errcode: 0, message: "Layers retrieved successfully.", layers: [';
-	if ($row = mysql_fetch_array($result)) {
-		echo '{ id: '.$row['id'].', visible: true, title: '.json_encode($row['title']).', summary: '.json_encode($row['summary']).', description: '.json_encode($row['description']).' }';
+/* Encode layer details. */
+function encode_layer($l) {
+	return '{i:'.$l['id'].',v:true,t:'.json_encode($l['title']).',s:'.json_encode($l['summary']).',d:'.json_encode($l['description']).'}';
+}
+
+/* Encode layers. */
+function encode_layers($lyrs) {
+	$str = '[';
+	if ($l = mysql_fetch_array($lyrs)) {
+		$str .= encode_layer($l);
 	}
-	while ($row = mysql_fetch_array($result)) {
-		echo ', { id: '.$row['id'].', visible: true, title: '.json_encode($row['title']).', summary: '.json_encode($row['summary']).', description: '.json_encode($row['description']).' }';
+	while ($l = mysql_fetch_array($lyrs)) {
+		$str .= ','.encode_layer($l);
 	}
-	echo ']}';
+	$str .=']';
+	return $str;
+}
+
+
+$logged_in = checkLogin();
+if (!$logged_in) {
+	header('Location: ngembryo.php');
 } else {
-	echo '<response><success>true</success><errcode>0</errcode><message>Layers retrieved successfully.</message><layers>';
-	while ($row = mysql_fetch_array($result)) {
-		echo '<layer><id>'.$row['id'].'</id><visible>false</visible><title>'.$row['title'].'</title><summary>'.$row['summary'].'</summary><description>'.$row['description'].'</description></layer>';
-	}
-	echo '</layers></response>';
+
+	/* Supplied by the client. */
+	$model = $_GET[model];
+	$yaw = $_GET[yaw];
+	$pitch = $_GET[pitch];
+	$roll = $_GET[roll];
+	$distance = $_GET[distance];
+
+	$oid = find_orientation($model, $yaw, $pitch, $roll, $distance);
+	$lyrs = get_layers($oid);
+	$json = encode_layers($lyrs);
+	echo_success("Layers retrieved successfully.", $json);
 }
 
 mysql_close($con);
