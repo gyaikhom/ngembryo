053dc229ac096fcb298a0b0cc2c26390c4687cb7
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-07-27 11:55:14 +0000

    * Removed alert. * Code refinements and dded session requirement.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@167 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- arta.php
+++ arta.php
@@ -1,57 +1,114 @@
 <?php
-$con = mysql_connect("localhost", "ngembryo", "ngembryo");
-if (!$con) {
-	die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', id: 0}');
+/**
+ * @projectDescription The Next-Generation Embryology Project
+ *
+ * School of Informatics, University of Edinburgh Funded by the JISC
+ * (http://www.jisc.ac.uk/)
+ *
+ * @author gyaikhom
+ *
+ * @description Add resource to annotation.
+ */
+
+include 'login.php';
+
+function die_error($c, $m) {
+	die('{success: false, errcode: '.$c.', message: "'.$m.'", id: 0}');
 }
 
-mysql_select_db("ngembryo", $con);
-
-/* Supplied by the client. */
-$rid = $_GET[rid];
-$aid = $_GET[aid];
-$type = $_GET[type];
-$table = "";
-
-if ($type == "m") {
-	$table = "2Dmarker";
-} else {
-	if ($type == "r") {
-		$table = "2Dregion";
-	} else {
-		die('{success: false, errcode: 2, message: "Unknown annotation type.", id: 0}');
-	}
+function echo_success($m, $id) {
+	echo '{success: true, errcode: 0, message: "'.$m.'", id:'.$id.'}';
 }
 
 /* Check if the resource exists. */
-$resource = mysql_query("SELECT id FROM resource WHERE deleted_at IS NULL AND id=$rid");
-if ($temp = mysql_fetch_array($resource)) {
-	$rid = $temp['id'];
-} else {
-	die('{success: false, errcode: -1, message: "Supplied resource does not exists.", id: 0}');
+function check_resource($rid) {
+	global $con;
+	$sql = "SELECT id FROM resource WHERE deleted_at IS NULL AND id=$rid LIMIT 1";
+	if (!($temp = mysql_query($sql, $con))) {
+		die_error(-1, json_encode(mysql_error()));
+	}
+	if (mysql_num_rows($temp) > 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 /* Check if the annotation exists. */
-$annotation = mysql_query("SELECT id FROM $table WHERE deleted_at IS NULL AND id=$aid");
-if ($temp = mysql_fetch_array($annotation)) {
-	$aid = $temp['id'];
-} else {
-	die('{success: false, errcode: -1, message: "Supplied annotation does not exists.", id: 0}');
+function check_annotation($table, $aid) {
+	global $con;
+	$sql = "SELECT id FROM $table WHERE deleted_at IS NULL AND id=$aid LIMIT 1";
+	if (!($temp = mysql_query($sql, $con))) {
+		die_error(-2, json_encode(mysql_error()));
+	}
+	if (mysql_num_rows($temp) > 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 /* Check if the resource is already linked to the annotation. */
-$table = $table."Resource";
-$already = mysql_query("SELECT id FROM $table WHERE deleted_at IS NULL AND annotation_id=$aid AND resource_id=$rid");
-if ($temp = mysql_fetch_array($already)) {
-	die('{success: false, errcode: -1, message: "Resource already linked to the annotation.", id: 0}');
+function check_linked($table, $aid, $rid) {
+	global $con;
+	$sql = "SELECT id FROM $table WHERE deleted_at IS NULL AND annotation_id=$aid AND resource_id=$rid LIMIT 1";
+	if (!($temp = mysql_query($sql, $con))) {
+		die_error(-3, json_encode(mysql_error()));
+	}
+	if (mysql_num_rows($temp) > 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 /* Add resource to annotation */
-$sql = "INSERT INTO $table (annotation_id, resource_id, created_at) VALUES ('$aid', '$rid', NOW())";
-if (!mysql_query($sql, $con)) {
-	die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', id: 0}');
+function link_resource_to_annotation($table, $aid, $rid) {
+	global $con;
+	$sql = "INSERT INTO $table (annotation_id, resource_id, created_at) VALUES ('$aid', '$rid', NOW())";
+	if (!mysql_query($sql, $con)) {
+		die_error(-4, json_encode(mysql_error()));
+	}
+	return mysql_insert_id();
 }
-$id = mysql_insert_id();
-echo '{success: true, errcode: 0, message: "Resource added to annotation.", id:'.$id.'}';
+
+$logged_in = checkLogin();
+if (!$logged_in) {
+	header('Location: ngembryo.php');
+} else {
+
+	/* Supplied by the client. */
+	$rid = $_GET[rid];
+	$aid = $_GET[aid];
+	$type = $_GET[type];
+	$table = "";
+
+	if ($type == "m") {
+		$table = "2Dmarker";
+	} else {
+		if ($type == "r") {
+			$table = "2Dregion";
+		} else {
+			die_error(1, "Unknown annotation type.");
+		}
+	}
+
+	if (check_resource($rid)) {
+		if (check_annotation($table, $aid)) {
+			$table = $table."Resource";
+			if (!check_linked($table, $aid, $rid)) {
+				$id = link_resource_to_annotation($table, $aid, $rid);
+				echo_success("Resource added to annotation.", $id);
+			} else {
+				die_error(2, "Resource already linked to the annotation.");
+			}
+		} else {
+			die_error(3, "Supplied annotation does not exists.");
+		}
+	} else {
+		die_error(4, "Supplied resource does not exists.");
+	}
+}
 
 mysql_close($con);
 ?>
