65828c9eb387ed6a7cad4eee5af7a07e8c375976
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-07-29 15:56:58 +0000

    * Get orientations: code refinement and session requirement. * Added ownership information.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@193 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- go.php
+++ go.php
@@ -1,40 +1,65 @@
 <?php
-$con = mysql_connect("localhost", "ngembryo", "ngembryo");
-if (!$con) {
-	if ($_GET[format] == "json") {
-		die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', orientations: null}');
-	} else {
-		echo '<response><success>false</success><errcode>1</errcode><message>'.mysql_error().'</message><orientations></orientations></response>';
-	}
+/**
+ * @projectDescription The Next-Generation Embryology Project
+ *
+ * School of Informatics, University of Edinburgh Funded by the JISC
+ * (http://www.jisc.ac.uk/)
+ *
+ * @author gyaikhom
+ *
+ * @description Get orientations.
+ */
+
+include 'login.php';
+
+function die_error($c, $m) {
+	die('{success:false,errcode:'.$c.',message:"'.$m.'",o:null}');
 }
-mysql_select_db("ngembryo", $con);
-$model = $_GET[model];
+
+function echo_success($m, $o) {
+	echo '{success:true,errcode:0,message:"'.$m.'",o:'.$o.'}';
+}
 
 /* Find all of the orientations for this model. */
-$sql = "SELECT * FROM orientation WHERE deleted_at IS NULL AND model_id=$model";
-if (!($result = mysql_query($sql, $con))) {
-	if ($_GET[format] == "json") {
-		die('{success: false, errcode: 2, message: '.json_encode(mysql_error()).', orientations: null}');
+function find_orientation($u, $mid) {
+	global $con;
+	$sql = "SELECT * FROM orientation WHERE deleted_at IS NULL AND owner='$u' AND model_id=$mid";
+	if (($t = mysql_query($sql, $con))) {
+		return $t;
 	} else {
-        echo '<response><success>false</success></errcode>2</errcode><message>'.mysql_error().'</message><orientations></orientations></response>';
+		die_error(-1, json_encode(mysql_error()));
 	}
 }
 
-if ($_GET[format] == "json") {
-	echo '{success: true, errcode: 0, message: "Orientations retrieved successfully.", orientations: [';
-	if ($row = mysql_fetch_array($result)) {
-		echo '{ id: '.$row['id'].', title: '.json_encode($row['title']).', description: '.json_encode($row['description']).', yaw: '.$row['yaw'].', pitch: '.$row['pitch'].', roll: '.$row['roll'].', distance: '.$row['distance'].'}';
+/* Encode orientation. */
+function encode_orientation($o) {
+	return '{id:'.$o['id'].',t:'.json_encode($o['title']).',d:'.json_encode($o['description']).',y:'.$o['yaw'].',p:'.$o['pitch'].',r:'.$o['roll'].',ds:'.$o['distance'].'}';
+}
+
+/* Encode orientations. */
+function encode_orientations($os) {
+	$str = '[';
+	if ($o = mysql_fetch_array($os)) {
+		$str .= encode_orientation($o);
+		while ($o = mysql_fetch_array($os)) {
+			$str .= ','.encode_orientation($o);
+		}
 	}
-	while ($row = mysql_fetch_array($result)) {
-		echo ', { id: '.$row['id'].', title: '.json_encode($row['title']).', description: '.json_encode($row['description']).', yaw: '.$row['yaw'].', pitch: '.$row['pitch'].', roll: '.$row['roll'].', distance: '.$row['distance'].'}';
-	}
-	echo ']}';
+	return $str.']';
+}
+
+$logged_in = checkLogin();
+if (!$logged_in) {
+	header('Location: ngembryo.php');
 } else {
-	echo '<response><success>true</success><errcode>0</errcode><message>Orientations retrieved successfully.</message><orientations>';
-	while ($row = mysql_fetch_array($result)) {
-		echo '<orientation><id>'.$row['id'].'</id><title>'.$row['title'].'</title><description>'.$row['description'].'</description><yaw>'.$row['yaw'].'</yaw><pitch>'.$row['pitch'].'</pitch><roll>'.$row['roll'].'</roll><distance>'.$row['distance'].'</distance></orientation>';
-	}
-	echo '</orientations></response>';
+
+	/* Supplied by the client. */
+	$model = $_GET[model];
+	$user = $_SESSION['username'];
+
+	$os = find_orientation($user, $model);
+	$json = encode_orientations($os);
+	echo_success("Orientations retrieved successfully.", $json);
 }
 
 mysql_close($con);
