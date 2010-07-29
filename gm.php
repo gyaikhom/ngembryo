66d54b4f2ca3644323f49877f269ca8122bf7a02
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-07-29 11:22:41 +0000

    Get markers: code refinement and session requirement.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@185 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- gm.php
+++ gm.php
@@ -1,88 +1,136 @@
 <?php
-$con = mysql_connect("localhost", "ngembryo", "ngembryo");
-if (!$con) {
-	if ($_GET[format] == "json") {
-		die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', markers: null}');
-	} else {
-		echo '<response><success>false</success><errcode>1</errcode><message>MySQL connection error:'.mysql_error().'</message><markers></markers></response>';
-	}
-}
-mysql_select_db("ngembryo", $con);
-$lid = $_GET[lid];
+/**
+ * @projectDescription The Next-Generation Embryology Project
+ *
+ * School of Informatics, University of Edinburgh Funded by the JISC
+ * (http://www.jisc.ac.uk/)
+ *
+ * @author gyaikhom
+ *
+ * @description Get markers and resources.
+ *
+ * NOTE: This is the new version according to the requirement from April 30,
+ * meeting at NCL. No more resource items. Only a flat list of resources.
+ */
 
-/* First check if the layer exists. */
-$layer = mysql_query("SELECT id FROM layer WHERE deleted_at IS NULL AND id=$lid");
-if ($temp = mysql_fetch_array($layer)) {
-	$lid = $temp['id'];
-} else {
-	if ($_GET[format] == "json") {
-		die('{success: false, errcode: -1, message: "Invalid layer.", markers: null}');
+include 'login.php';
+
+function die_error($c, $m) {
+	die('{success:false,errcode:'.$c.',message:"'.$m.'",m:null}');
+}
+
+function echo_success($m, $r) {
+	echo '{success:true,errcode:0,message:"'.$m.'",m:'.$r.'}';
+}
+
+/* Check if a layer with the given id exists. */
+function check_layer($lid) {
+	global $con;
+	$sql = "SELECT id FROM layer WHERE deleted_at IS NULL AND id=$lid LIMIT 1";
+	if ($temp = mysql_query($sql, $con)) {
+		if (mysql_num_rows($temp) > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	} else {
-		echo '<response><success>false</success><errcode>-1</errcode><message>Invalid layer</message><markers></markers></response>';
+		die_error(-1, json_encode(mysql_error()));
 	}
 }
 
 /* Find all of the markers for this layer. */
-$sql = "SELECT * FROM 2Dmarker WHERE deleted_at IS NULL AND layer_id=$lid AND x >= '$_GET[x_low]' AND x <= '$_GET[x_high]' AND y >= '$_GET[y_low]' AND y <= '$_GET[y_high]' AND scale <= '$_GET[scale_high]' AND scale >= '$_GET[scale_low]'";
-if (!($result = mysql_query($sql, $con))) {
-	if ($_GET[format] == "json") {
-		die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', markers: null}');
+function find_markers($lid, $xl, $xh, $yl, $yh, $sl, $sh) {
+	global $con;
+	$sql = "SELECT * FROM 2Dmarker WHERE deleted_at IS NULL AND layer_id=$lid AND x >= '$xl' AND x <= '$xh' AND y >= '$yl' AND y <= '$yh' AND scale <= '$sh' AND scale >= '$sl'";
+	if (!($temp = mysql_query($sql, $con))) {
+		die_error(-2, json_encode(mysql_error()));
+	}
+	return $temp;
+}
+
+/* Get number of resources connected to this marker. */
+function get_resources_count($aid) {
+	global $con;
+	$sql = "SELECT DISTINCT resource.id FROM resource LEFT JOIN 2DmarkerResource ON 2DmarkerResource.resource_id=resource.id WHERE resource.deleted_at IS NULL AND 2DmarkerResource.annotation_id IS NOT NULL AND 2DmarkerResource.annotation_id=$aid";
+	if (($temp = mysql_query($sql, $con))) {
+		return mysql_num_rows($temp);
 	} else {
-		echo '<response><success>false</success><errcode>1</errcode><message>MySQL Query error:'.mysql_error().'</message><markers></markers></response>';
+		die_error(-3, json_encode(mysql_error()));
 	}
 }
 
-/**
- * This is the new version according to the requirement from April 30, meeting at NCL.
- * No more resource items. Only a flat list of resources.
- */
-function listResources($aid) {
-    echo ', resources: { count: ';
-	$count = mysql_query("SELECT DISTINCT COUNT(*) FROM resource LEFT JOIN 2DmarkerResource ON 2DmarkerResource.resource_id=resource.id WHERE resource.deleted_at IS NULL AND 2DmarkerResource.annotation_id IS NOT NULL AND 2DmarkerResource.annotation_id=$aid");
-    if ($temp = mysql_fetch_array($count)) {
-    	echo $temp['COUNT(*)'];
-    } else {
-    	echo '0';
-    }
-    echo ', resources: [';
-	$resources = mysql_query("SELECT DISTINCT resource.title, resource.author FROM resource LEFT JOIN 2DmarkerResource ON 2DmarkerResource.resource_id=resource.id WHERE resource.deleted_at IS NULL AND 2DmarkerResource.annotation_id IS NOT NULL AND 2DmarkerResource.annotation_id=$aid LIMIT 5");
-	if ($resource = mysql_fetch_array($resources)) {
-		echo "{ title: ".json_encode($resource['title']).", author: ".json_encode($resource['author'])."}";
-	}
-	while ($resource = mysql_fetch_array($resources)) {
-		echo ", { title: ".json_encode($resource['title']).", author: ".json_encode($resource['author'])."}";
-	}
-	echo ']}}';
+/* Encode resource. */
+function encode_resource($r) {
+	return "{t:".json_encode($r['title']).",a:".json_encode($r['author'])."}";
 }
 
-if ($_GET[format] == "json") {
-	echo '{success: true, errcode: 0, message: "Markers retrieved successfully.", markers: [';
-	if ($row = mysql_fetch_array($result)) {
-		echo '{ id: '.$row['id'].', x: '.$row['x'].', y: '.$row['y'].', scale: '.$row['scale'].', label: '.json_encode($row['label']).', description: '.json_encode($row['description']);
-		listResources($row['id']);
-	}
 
-	/**
-	 * Old version.
-	 *
-	 echo '{ id: '.$row['id'].', x: '.$row['x'].', y: '.$row['y'].', scale: '.$row['scale'].', label: '.json_encode($row['label']).', description: '.json_encode($row['description']).' }';
-	 while ($row = mysql_fetch_array($result)) {
-		echo ', { id: '.$row['id'].', x: '.$row['x'].', y: '.$row['y'].', scale: '.$row['scale'].', label: '.json_encode($row['label']).', description: '.json_encode($row['description']).' }';
+/* Get resources. */
+function get_resources($aid) {
+	global $con;
+	$sql = "SELECT DISTINCT resource.id, resource.title, resource.author FROM resource LEFT JOIN 2DmarkerResource ON 2DmarkerResource.resource_id=resource.id WHERE resource.deleted_at IS NULL AND 2DmarkerResource.annotation_id IS NOT NULL AND 2DmarkerResource.annotation_id=$aid LIMIT 5";
+	if (($t = mysql_query($sql, $con))) {
+		if ($r = mysql_fetch_array($t)) {
+			$str = '['.encode_resource($r);
 		}
-		echo ']}';*/
-
-	while ($row = mysql_fetch_array($result)) {
-		echo ', { id: '.$row['id'].', x: '.$row['x'].', y: '.$row['y'].', scale: '.$row['scale'].', label: '.json_encode($row['label']).', description: '.json_encode($row['description']);
-		listResources($row['id']);
+		while ($r = mysql_fetch_array($t)) {
+			$str .= ','.encode_resource($r);
+		}
+		$str .= ']';
+		return $str;
+	} else {
+		die_error(-4, json_encode(mysql_error()));
 	}
-	echo ']}';
-
-} else {
-	echo '<response><success>true</success><errcode>0</errcode><message>Markers retrieved successfully.</message><markers>';
-	while ($row = mysql_fetch_array($result)) {
-		echo '<marker><id>'.$row['id'].'</id><x>'.$row['x'].'</x><y>'.$row['y'].'</y><scale>'.$row['scale'].'</scale><label>'.$row['label'].'</label><description>'.$row['description'].'</description></marker>';
-	}
-	echo '</markers></response>';
 }
+
+/* Encode marker. */
+function encode_marker($m) {
+	$str =  '{id:'.$m['id'].',x:'.$m['x'].',y:'.$m['y'].',s:'.$m['scale'].',l:'.json_encode($m['label']).',d:'.json_encode($m['description']);
+	$aid = $m['id'];
+	if (($c = get_resources_count($aid)) > 0) {
+		$str .= ',r:{c:'.$c.',r:'.get_resources($aid).'}}';
+	} else {
+		$str .= ',r:{c:0,r:null}}';
+	}
+	return $str;
+}
+
+/* Encode markers and resources. */
+function encode_markers($mkrs) {
+	$str = "[";
+
+	if ($m = mysql_fetch_array($mkrs)) {
+		$str .= encode_marker($m);
+	}
+	while ($m = mysql_fetch_array($mkrs)) {
+		$str .= ','.encode_marker($m);
+	}
+	$str .= ']';
+	return $str;
+}
+
+$logged_in = checkLogin();
+if (!$logged_in) {
+	header('Location: ngembryo.php');
+} else {
+
+	/* Supplied by the client. */
+	$lid = $_GET[lid];
+	$xl = $_GET[x_low];
+	$xh = $_GET[x_high];
+	$yl = $_GET[y_low];
+	$yh = $_GET[y_high];
+	$sl = $_GET[scale_low];
+	$sh = $_GET[scale_high];
+
+	if (check_layer($lid)) {
+		$mkrs = find_markers($lid, $xl, $xh, $yl, $yh, $sl, $sh);
+		$json = encode_markers($mkrs);
+	} else {
+		die_error(-5, "Invalid layer.");
+	}
+	echo_success("Markers retrieved successfully.", $json);
+}
+
 mysql_close($con);
 ?>
