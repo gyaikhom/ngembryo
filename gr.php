8411ae2cd6abcfca954e0395b19226cb7ab34dd5
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-07-29 12:24:17 +0000

    Get regions: code refinement and session requirement.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@187 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- gr.php
+++ gr.php
@@ -1,102 +1,161 @@
 <?php
-$con = mysql_connect("localhost", "ngembryo", "ngembryo");
-if (!$con) {
-	if ($_GET[format] == "json") {
-		die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', regions: null}');
-	} else {
-		echo '<response><success>false</success><errcode>1</errcode><message>MySQL connection error:'.mysql_error().'</message><regions></regions></response>';
-	}
-}
-mysql_select_db("ngembryo", $con);
-$lid = $_GET[lid];
+/**
+ * @projectDescription The Next-Generation Embryology Project
+ *
+ * School of Informatics, University of Edinburgh Funded by the JISC
+ * (http://www.jisc.ac.uk/)
+ *
+ * @author gyaikhom
+ *
+ * @description Get regions and resources.
+ *
+ * NOTE: This is the new version according to the requirement from April 30,
+ * meeting at NCL. No more resource items. Only a flat list of resources.
+ */
 
-/* First check if the layer exists. */
-$layer = mysql_query("SELECT id FROM layer WHERE deleted_at IS NULL AND id=$lid");
-if ($temp = mysql_fetch_array($layer)) {
-	$lid = $temp['id'];
-} else {
-	if ($_GET[format] == "json") {
-		die('{success: false, errcode: -1, message: "Invalid layer.", regions: null}');
+include 'login.php';
+
+function die_error($c, $m) {
+	die('{success:false,errcode:'.$c.',message:"'.$m.'",r:null}');
+}
+
+function echo_success($m, $r) {
+	echo '{success:true,errcode:0,message:"'.$m.'",r:'.$r.'}';
+}
+
+/* Check if a layer with the given id exists. */
+function check_layer($lid) {
+	global $con;
+	$sql = "SELECT id FROM layer WHERE deleted_at IS NULL AND id=$lid LIMIT 1";
+	if ($temp = mysql_query($sql, $con)) {
+		if (mysql_num_rows($temp) > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	} else {
-		echo '<response><success>false</success><errcode>-1</errcode><message>Invalid layer</message><regions></regions></response>';
+		die_error(-1, json_encode(mysql_error()));
 	}
 }
 
 /* Find all of the regions for this layer. */
-$sql = "SELECT * FROM 2Dregion WHERE deleted_at IS NULL AND layer_id='$lid' AND scale <= '$_GET[scale_high]' AND scale >= '$_GET[scale_low]' AND ((tl_x >= '$_GET[x_low]' AND tl_x <= '$_GET[x_high]' AND tl_y >= '$_GET[y_low]' AND tl_y <= '$_GET[y_high]') OR (br_x >= '$_GET[x_low]' AND br_x <= '$_GET[x_high]' AND br_y >= '$_GET[y_low]' AND br_y <= '$_GET[y_high]') OR (tl_x < '$_GET[x_low]' AND br_x > '$_GET[x_high]' AND ((br_y >= '$_GET[y_low]' AND br_y <= '$_GET[y_high]') OR (tl_y >= '$_GET[y_low]' AND tl_y <= '$_GET[y_high]') OR (tl_y < '$_GET[y_low]' AND br_y > '$_GET[y_high]'))) OR (tl_y < '$_GET[y_low]' AND br_y > '$_GET[y_high]' AND ((br_x >= '$_GET[x_low]' AND br_x <= '$_GET[x_high]') OR (tl_x >= '$_GET[x_low]' AND tl_x <= '$_GET[x_high]') OR (tl_x < '$_GET[x_low]' AND br_x > '$_GET[x_high]'))))";
-if (!($result = mysql_query($sql, $con))) {
-	if ($_GET[format] == "json") {
-		die('{success: false, errcode: 1, message: '.json_encode(mysql_error()).', regions: null}');
+function find_regions($lid, $xl, $xh, $yl, $yh, $sl, $sh) {
+	global $con;
+	$sql = "SELECT * FROM 2Dregion WHERE deleted_at IS NULL AND layer_id='$lid' AND scale <= '$sh' AND scale >= '$sl' AND ((tl_x >= '$xl' AND tl_x <= '$xh' AND tl_y >= '$yl' AND tl_y <= '$yh') OR (br_x >= '$xl' AND br_x <= '$xh' AND br_y >= '$yl' AND br_y <= '$yh') OR (tl_x < '$xl' AND br_x > '$xh' AND ((br_y >= '$yl' AND br_y <= '$yh') OR (tl_y >= '$yl' AND tl_y <= '$yh') OR (tl_y < '$yl' AND br_y > '$yh'))) OR (tl_y < '$yl' AND br_y > '$yh' AND ((br_x >= '$xl' AND br_x <= '$xh') OR (tl_x >= '$xl' AND tl_x <= '$xh') OR (tl_x < '$xl' AND br_x > '$xh'))))";
+	if (!($temp = mysql_query($sql, $con))) {
+		die_error(-2, json_encode(mysql_error()));
+	}
+	return $temp;
+}
+
+/* Get number of resources connected to this region. */
+function get_resources_count($aid) {
+	global $con;
+	$sql = "SELECT DISTINCT resource.id FROM resource LEFT JOIN 2DregionResource ON 2DregionResource.resource_id=resource.id WHERE resource.deleted_at IS NULL AND 2DregionResource.annotation_id IS NOT NULL AND 2DregionResource.annotation_id=$aid";
+	if (($temp = mysql_query($sql, $con))) {
+		return mysql_num_rows($temp);
 	} else {
-		echo '<response><success>false</success><errcode>1</errcode><message>MySQL Query error:'.mysql_error().'</message><regions></regions></response>';
+		die_error(-3, json_encode(mysql_error()));
 	}
 }
 
-/**
- * This is the new version according to the requirement from April 30, meeting at NCL.
- * No more resource items. Only a flat list of resources.
- */
-function listResources($aid) {
-	echo ', resources: { count: ';
-	$count = mysql_query("SELECT DISTINCT COUNT(*) FROM resource LEFT JOIN 2DregionResource ON 2DregionResource.resource_id=resource.id WHERE resource.deleted_at IS NULL AND 2DregionResource.annotation_id IS NOT NULL AND 2DregionResource.annotation_id=$aid");
-	if ($temp = mysql_fetch_array($count)) {
-		echo $temp['COUNT(*)'];
-	} else {
-		echo '0';
-	}
-
-	echo ', resources: [';
-	$resources = mysql_query("SELECT DISTINCT resource.title, resource.author FROM resource LEFT JOIN 2DregionResource ON 2DregionResource.resource_id=resource.id WHERE resource.deleted_at IS NULL AND 2DregionResource.annotation_id IS NOT NULL AND 2DregionResource.annotation_id=$aid LIMIT 5");
-	if ($resource = mysql_fetch_array($resources)) {
-		echo "{ title: ".json_encode($resource['title']).", author: ".json_encode($resource['author'])."}";
-	}
-	while ($resource = mysql_fetch_array($resources)) {
-		echo ", { title: ".json_encode($resource['title']).", author: ".json_encode($resource['author'])."}";
-	}
-	echo ']}';
+/* Encode resource. */
+function encode_resource($r) {
+	return "{t:".json_encode($r['title']).",a:".json_encode($r['author'])."}";
 }
 
-function printRegion($region) {
-    /* echo '{ id: '.$region['id'].', scale: '.$region['scale'].', tl_x: '.$region['tl_x'].', tl_y: '.$region['tl_y'].', br_x: '.$region['br_x'].', br_y: '.$region['br_y'].', label: '.json_encode($region['label']).', description: '.json_encode($region['description']); */
-	echo '{ id: '.$region['id'].', scale: '.$region['scale'].', label: '.json_encode($region['label']).', description: '.json_encode($region['description']);
-	listResources($region['id']);
-	echo ', polyline: ';
-	$polyline = mysql_query("SELECT * FROM 2Dpolyline WHERE deleted_at IS NULL AND 2Dregion_id='".$region['id']."' ORDER BY rank ASC");
-	if ($point = mysql_fetch_array($polyline)) {
-		$count = 1;
-		/* echo '[['.$point['x'].','.$point['y'].','.$point['rank'].']'; */
-		echo '[{x:'.$point['x'].',y:'.$point['y'].'}';
-	}
-	while ($point = mysql_fetch_array($polyline)) {
-		/* echo ',['.$point['x'].','.$point['y'].','.$point['rank'].']'; */
-		echo ',{x:'.$point['x'].',y:'.$point['y'].'}';
-		$count++;
-	}
-	if ($count > 0)
-	echo ']';
-	echo ' }';
-}
 
-if ($_GET[format] == "json") {
-	echo '{success: true, errcode: 0, message: "Regions retrieved successfully.", regions: [';
-	if ($region = mysql_fetch_array($result))
-	printRegion($region);
-	while ($region = mysql_fetch_array($result)) {
-		echo ',';
-		printRegion($region);
-	}
-	echo ']}';
-} else {
-	echo '<response><success>true</success><errcode>0</errcode><message>Regions retrieved successfully.</message><regions>';
-	while ($row = mysql_fetch_array($result)) {
-		echo '<region><id>'.$region['id'].'</id><scale>'.$region['scale'].'</scale><tl_x>'.$region['tl_x'].'</tl_x><tl_y>'.$region['tl_y'].'</tl_y><br_x>'.$region['br_x'].'</br_x><br_y>'.$region['br_y'].'</br_y><label>'.$region['label'].'</label><description>'.$region['description'].'</description><polyline>';
-		$polyline = mysql_query("SELECT * FROM 2Dpolyline WHERE deleted_at IS NULL AND 2Dregion_id='".$region['id']."' ORDER BY rank ASC");
-		while ($point = mysql_fetch_array($polyline)) {
-			echo '<point><x>'.$point['x'].'</x><y>'.$point['y'].'</y><rank>'.$point['rank'].'</rank></point>';
+/* Get resources. */
+function get_resources($aid) {
+	global $con;
+	$sql = "SELECT DISTINCT resource.id, resource.title, resource.author FROM resource LEFT JOIN 2DregionResource ON 2DregionResource.resource_id=resource.id WHERE resource.deleted_at IS NULL AND 2DregionResource.annotation_id IS NOT NULL AND 2DregionResource.annotation_id=$aid LIMIT 5";
+	if (($t = mysql_query($sql, $con))) {
+		if ($r = mysql_fetch_array($t)) {
+			$str = '['.encode_resource($r);
 		}
-		echo '</polyline></region>';
+		while ($r = mysql_fetch_array($t)) {
+			$str .= ','.encode_resource($r);
+		}
+		$str .= ']';
+		return $str;
+	} else {
+		die_error(-4, json_encode(mysql_error()));
 	}
-	echo '</regions></response>';
+}
+
+/* Encode point. */
+function encode_point($p) {
+	return '{x:'.$p['x'].',y:'.$p['y'].'}';
+}
+
+/* Encode polyline. */
+function encode_polyline($aid) {
+	global $con;
+	$str = '[';
+	$sql = "SELECT * FROM 2Dpolyline WHERE deleted_at IS NULL AND 2Dregion_id=$aid ORDER BY rank ASC";
+	if (($t = mysql_query($sql, $con))) {
+		if ($p = mysql_fetch_array($t)) {
+			$str .= encode_point($p);
+		}
+		while ($p = mysql_fetch_array($t)) {
+			$str .= ','.encode_point($p);
+		}
+	} else {
+		die_error(-4, json_encode(mysql_error()));
+	}
+	$str .= ']';
+	return $str;
+
+}
+
+/* Encode region. */
+function encode_region($r) {
+	$str =  '{id:'.$r['id'].',s:'.$r['scale'].',l:'.json_encode($r['label']).',d:'.json_encode($r['description']);
+	$aid = $r['id'];
+	$str .= ',p:'.encode_polyline($aid);
+	if (($c = get_resources_count($aid)) > 0) {
+		$str .= ',r:{c:'.$c.',r:'.get_resources($aid).'}}';
+	} else {
+		$str .= ',r:{c:0,r:null}}';
+	}
+	return $str;
+}
+
+/* Encode regions and resources. */
+function encode_regions($rgs) {
+	$str = "[";
+
+	if ($r = mysql_fetch_array($rgs)) {
+		$str .= encode_region($r);
+	}
+	while ($r = mysql_fetch_array($rgs)) {
+		$str .= ','.encode_region($r);
+	}
+	$str .= ']';
+	return $str;
+}
+
+$logged_in = checkLogin();
+if (!$logged_in) {
+	header('Location: ngembryo.php');
+} else {
+
+	/* Supplied by the client. */
+	$lid = $_GET[lid];
+	$xl = $_GET[x_low];
+	$xh = $_GET[x_high];
+	$yl = $_GET[y_low];
+	$yh = $_GET[y_high];
+	$sl = $_GET[scale_low];
+	$sh = $_GET[scale_high];
+
+	if (check_layer($lid)) {
+		$rgs = find_regions($lid, $xl, $xh, $yl, $yh, $sl, $sh);
+		$json = encode_regions($rgs);
+	} else {
+		die_error(-5, "Invalid layer.");
+	}
+	echo_success("Regions retrieved successfully.", $json);
 }
 
 mysql_close($con);
