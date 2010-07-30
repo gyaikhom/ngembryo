0132e2a8d90e3ce7eba169ca4d469731d7d68b9b
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-07-30 14:46:53 +0000

    Added facility for editing annotation details.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@205 b13ead23-0dcf-405c-9d34-bbe0b799025b

---
+++ ea.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @projectDescription The Next-Generation Embryology Project
+ *
+ * School of Informatics, University of Edinburgh Funded by the JISC
+ * (http://www.jisc.ac.uk/)
+ *
+ * @author gyaikhom
+ *
+ * @description Save changes to annotation details.
+ */
+
+include 'login.php';
+
+function die_error($c, $m) {
+	die('{success: false, errcode: '.$c.', message: "'.$m.'"}');
+}
+
+function echo_success($m) {
+	echo '{success: true, errcode: 0, message: "'.$m.'"}';
+}
+
+/* Check if an annotation with the given label already exists. */
+function check_annotation($u, $aid, $table, $label) {
+	global $con;
+	$sql = "SELECT id FROM $table WHERE deleted_at IS NULL AND owner='$u' AND label='$label' LIMIT 1";
+	if ($temp = mysql_query($sql, $con)) {
+		if (mysql_num_rows($temp) > 0) {
+			if ($k = mysql_fetch_array($temp)) {
+				if ($k[0] == $aid) {
+					return false;
+				} else {
+					return true;
+				}
+			}
+		} else {
+			return false;
+		}
+	} else {
+		die_error(-1, json_encode(mysql_error()));
+	}
+}
+
+/* Update annotation details. */
+function update_annotation($aid, $table, $l, $d) {
+	global $con;
+	$sql = "UPDATE $table SET label='$l',description='$d',updated_at=NOW() WHERE id=$aid";
+	if (!mysql_query($sql, $con)) {
+		die_error(-2, json_encode(mysql_error()));
+	}
+}
+
+$logged_in = checkLogin();
+if (!$logged_in) {
+	header('Location: ngembryo.php');
+} else {
+
+	/* Supplied by the client. */
+	$label = $_POST[label];
+	$description = $_POST[description];
+	$aid = $_POST[aid];
+	$type = $_POST[type];
+	$user = $_SESSION['username'];
+
+	if ($type == "m") {
+		$table = "2Dmarker";
+	} else {
+		if ($type == "r") {
+			$table = "2Dregion";
+		} else {
+			die_error(-3, "Unknown annotation type.");
+		}
+	}
+
+	/* Escape quotes etc. */
+	$label = return_well_formed($label);
+	$description = return_well_formed($description);
+
+	if (check_annotation($user, $aid, $table, $label)) {
+		die_error(-4, "Annotation \'$title\' already exists. Changes not saved.");
+	} else {
+		update_annotation($aid, $table, $label, $description);
+		echo_success("Changes to annotation \'$label\' have been saved.");
+	}
+}
+
+mysql_close($con);
+?>
