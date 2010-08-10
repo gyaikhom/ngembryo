384e363579e8aa39ee844a91bb512018c9088f11
gagarineyaikhom <gagarineyaikhom@b13ead23-0dcf-405c-9d34-bbe0b799025b>
2010-08-10 14:13:15 +0000

    Model details should be user specific.
    git-svn-id: svn://svn.code.sf.net/p/ngembryo/code@215 b13ead23-0dcf-405c-9d34-bbe0b799025b

--- gmd.php
+++ gmd.php
@@ -22,9 +22,9 @@ function die_error($c) {
 $details = array();
 
 /* Get annotation count. */
-function get_annotation_count($type, $id) {
+function get_annotation_count($type, $id, $u) {
 	global $con;
-	$sql = "SELECT id FROM $type WHERE deleted_at IS NULL AND layer_id=$id";
+	$sql = "SELECT id FROM $type WHERE deleted_at IS NULL AND owner='$u' AND layer_id=$id";
 	if (!($t = mysql_query($sql, $con))) {
 		die_error(1);
 	}
@@ -32,23 +32,23 @@ function get_annotation_count($type, $id) {
 }
 
 /* Get total annotation count. */
-function get_annotations($ls) {
+function get_annotations($ls, $u) {
 	global $con, $details;
 	foreach ($ls as $t) {
 		while ($l = mysql_fetch_array($t)) {
-			$details[0] += get_annotation_count("2Dmarker", $l[0]);
-			$details[1] += get_annotation_count("2Dregion", $l[0]);
+			$details[0] += get_annotation_count("2Dmarker", $l[0], $u);
+			$details[1] += get_annotation_count("2Dregion", $l[0], $u);
 		}
 	}
 }
 
 /* Get layer count. */
-function get_layers($os) {
+function get_layers($os, $u) {
 	global $con, $details;
 	$ls = array();
 	$c = 0;
 	while ($o = mysql_fetch_array($os)) {
-		$sql = "SELECT id FROM layer WHERE deleted_at IS NULL AND orientation_id=$o[0]";
+		$sql = "SELECT id FROM layer WHERE deleted_at IS NULL AND owner='$u' AND orientation_id=$o[0]";
 		if (!($ls[$c] = mysql_query($sql, $con))) {
 			die_error(2);
 		}
@@ -59,9 +59,9 @@ function get_layers($os) {
 }
 
 /* Get the orientation count. */
-function get_orientations($mid) {
+function get_orientations($mid, $u) {
 	global $con, $details;
-	$sql = "SELECT id FROM orientation WHERE deleted_at IS NULL AND model_id=$mid";
+	$sql = "SELECT id FROM orientation WHERE deleted_at IS NULL AND owner='$u' AND model_id=$mid";
 	if (!($os = mysql_query($sql, $con))) {
 		die_error(3);
 	}
@@ -70,10 +70,10 @@ function get_orientations($mid) {
 }
 
 /* Get model details. */
-function get_details($mid) {
-	$os = get_orientations($mid);
-	$ls = get_layers($os);
-	get_annotations($ls);
+function get_details($mid, $u) {
+	$os = get_orientations($mid, $u);
+	$ls = get_layers($os, $u);
+	get_annotations($ls, $u);
 }
 
 $logged_in = checkLogin();
@@ -85,7 +85,7 @@ if (!$logged_in) {
 	$details[1] = 0;
 	$details[2] = 0;
 	$details[3] = 0;
-	get_details($mid);
+	get_details($mid, $_SESSION['username']);
 	$d = "m:".$details[0].",r:".$details[1].",l:".$details[2].",o:".$details[3];
 	echo '{e:0,m:"Model details retrieved successfully.",d:{'.$d.'}}';
 }
